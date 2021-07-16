a410085a70a1594e005f12c6c192e7cf8209521f
Mohamed Ibrahim Ansari <54184745+ibrahimansar@users.noreply.github.com>
2021-07-16 17:10:10 +0530

    AutoCloseable imlpemented

--- src/main/java/Lucene/lucene/LuceneApp.java
+++ src/main/java/Lucene/lucene/LuceneApp.java
@@ -24,11 +24,13 @@ import org.apache.lucene.util.Version;
 import Lucene.lucene.CommandLine.Command;
 import Lucene.lucene.CommandLine.Option;
 
+
+
 @Command(name = "LuceneApp", description = "App to perform Lucene indexing and searching operations", mixinStandardHelpOptions = true, version = "LuceneApp 1.0")
-public class LuceneApp {
+public abstract class LuceneApp extends Indexing{
 	
 	@Option(names = "-path", description = "folder to be indexed; [usage: 'LuceneApp -path 'C:/User/data/' '] ")
-	static String path;
+	static String path = "lucene";
 	
     @Option(names = "-search", description = "Searches given name; [usage: 'LuceneApp -search 'Lucene' '] ")
 	static String Word;
@@ -39,15 +41,12 @@ public class LuceneApp {
     public static void main(String[] args) throws Exception {  
     	
     	File indexDir = new File("C:/Lucene/Index/");    	
-    	
     	//--indexing--//
     	if(path !=null && !path.isEmpty()) {
-        File dataDir = new File(path);
-//        File dataDir = new File("C:/Lucene/Data/");
+//        File dataDir = new File(path);
+        File dataDir = new File("C:/Lucene/Data/");
         String suffix = "txt";        
-        LuceneApp indexer = new LuceneApp();        
-        int numIndex = indexer.index(indexDir, dataDir, suffix);        
-        System.out.println("Numer of total files indexed:  " + numIndex);
+        close(indexDir, dataDir, suffix);        
         addPath(path);
     	}
         
@@ -56,8 +55,7 @@ public class LuceneApp {
         String query = Word;
 //        String query = "lucene";
         int hits = 100;        
-        LuceneApp searcher = new LuceneApp();
-        searcher.searchIndex(indexDir, query, hits);
+        searchIndex(indexDir, query, hits);
     	}
     	
     	//--List--//
@@ -67,19 +65,80 @@ public class LuceneApp {
 
     } 
     
-    //Indexing functions    
-	@SuppressWarnings("deprecation")
-	private int index(File indexDir, File dataDir, String suffix) throws Exception {        
+    //searching method    
+    private static void searchIndex(File indexDir, String queryStr, int maxHits) throws Exception {    
+    	try( Directory directory = FSDirectory.open(indexDir)){
+        @SuppressWarnings({ "deprecation", "resource" })
+		IndexSearcher searcher = new IndexSearcher(directory);
+        @SuppressWarnings("deprecation")
+		QueryParser parser = new QueryParser(Version.LUCENE_30, "contents", new SimpleAnalyzer());
+        Query query = parser.parse(queryStr);        
+        TopDocs topDocs = searcher.search(query, maxHits);
+    	
+        ScoreDoc[] hits = topDocs.scoreDocs;
+        for (int i = 0; i < hits.length; i++) {
+            int docId = hits[i].doc;
+            Document d = searcher.doc(docId);
+            System.out.println(d.get("filename"));
+        }
+        
+        System.out.println("Found " + hits.length);        
+      }
+      catch (FileNotFoundException e) {
+    	  System.out.println("Indexing directory not found");
+      }
+    }
+    
+    //List methods   
+    	//Adding path to list
+    private static void addPath(String path) {
+    	String fileName = "C:\\Lucene\\List.txt";
+        try {
+            BufferedWriter out = new BufferedWriter(new FileWriter(fileName, true));
+            out.write(path + "\n");
+            out.close();
+        }
+        catch (IOException e) {
+            System.out.println("exception occoured" + e);
+        }
+    }
+    
+    	//printing list
+    private static void printPaths() {
+    	String fileName = "C:\\Lucene\\List.txt";
+        try {
+            File myObj = new File(fileName);
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+              String data = myReader.nextLine();
+              System.out.println(data);
+            }
+            myReader.close();
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
+
+abstract class Indexing implements AutoCloseable{
+	public static void close(File indexDir, File dataDir, String suffix) throws Exception {
+		// TODO Auto-generated method stub
         IndexWriter indexWriter = new IndexWriter( FSDirectory.open(indexDir), new SimpleAnalyzer(), true, IndexWriter.MaxFieldLength.LIMITED);
-        indexWriter.setUseCompoundFile(false);        
-        indexDirectory(indexWriter, dataDir, suffix);        
+        indexWriter.setUseCompoundFile(false);     
+        IndexDir indexDirectory = new IndexDir();
+        indexDirectory.indexDirectory(indexWriter, dataDir, suffix);        
         int numIndexed = indexWriter.maxDoc();
         indexWriter.optimize();
         indexWriter.close();        
-        return numIndexed;        
-    }
-    
-    private void indexDirectory(IndexWriter indexWriter, File dataDir, String suffix) throws IOException {
+        System.out.println("Numer of total files indexed:  " + numIndexed);
+	}
+}
+
+
+class IndexDir {
+    public void indexDirectory(IndexWriter indexWriter, File dataDir, String suffix) throws IOException {
         File[] files = dataDir.listFiles();
         for (int i = 0; i < files.length; i++) {
             File f = files[i];
@@ -110,57 +169,5 @@ public class LuceneApp {
         doc.add(new Field("filename", f.getCanonicalPath(), Field.Store.YES, Field.Index.ANALYZED));
         
         indexWriter.addDocument(doc);
-    }
-    
-    //searching function    
-    private void searchIndex(File indexDir, String queryStr, int maxHits) throws Exception {        
-        Directory directory = FSDirectory.open(indexDir);
-        @SuppressWarnings({ "deprecation", "resource" })
-		IndexSearcher searcher = new IndexSearcher(directory);
-        @SuppressWarnings("deprecation")
-		QueryParser parser = new QueryParser(Version.LUCENE_30, "contents", new SimpleAnalyzer());
-        Query query = parser.parse(queryStr);        
-        TopDocs topDocs = searcher.search(query, maxHits);
-        
-        ScoreDoc[] hits = topDocs.scoreDocs;
-        for (int i = 0; i < hits.length; i++) {
-            int docId = hits[i].doc;
-            Document d = searcher.doc(docId);
-            System.out.println(d.get("filename"));
-        }
-        
-        System.out.println("Found " + hits.length);        
-    }
-    
-    //List functions   
-    	//Adding path to list
-    private static void addPath(String path) {
-    	String fileName = "C:\\Lucene\\List.txt";
-        try {
-            BufferedWriter out = new BufferedWriter(new FileWriter(fileName, true));
-            out.write(path + "\n");
-            out.close();
-        }
-        catch (IOException e) {
-            System.out.println("exception occoured" + e);
-        }
-    }
-    
-    	//printing list
-    private static void printPaths() {
-    	String fileName = "C:\\Lucene\\List.txt";
-        try {
-            File myObj = new File(fileName);
-            Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-              String data = myReader.nextLine();
-              System.out.println(data);
-            }
-            myReader.close();
-        }
-        catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
     }
 }
